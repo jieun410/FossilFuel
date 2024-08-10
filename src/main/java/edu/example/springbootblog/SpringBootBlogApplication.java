3562a918182cff9bf427e57b7fb387bb27086eb8
adorahelen <adorahelenmin@gmail.com>
2024-08-11 00:06:31 +0900

    Create Project .

---
+++ src/main/java/edu/example/springbootblog/SpringBootBlogApplication.java
@@ -0,0 +1,13 @@
+package edu.example.springbootblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBlogApplication.class, args);
+    }
+
+}
