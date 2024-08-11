bb675e23266faf7be14d02a7655c4c82fddb6e4c
adorahelen <adorahelenmin@gmail.com>
2024-08-11 15:21:57 +0900

    DTO become Entitiy

---
+++ src/main/java/edu/example/springbootblog/dto/AddArticleRequest.java
@@ -0,0 +1,24 @@
+package edu.example.springbootblog.dto;
+
+
+import edu.example.springbootblog.domain.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor // add general constructor
+@AllArgsConstructor // all filed value get parameter in constructor
+@Getter
+public class AddArticleRequest {
+
+    private String title;
+    private String content;
+
+    private Article toEntity(){
+        return Article.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
+// use to build pattern, DTO -> Entity (convert)
