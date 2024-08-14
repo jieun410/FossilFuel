3f11b0233d1c983b753ba3b97d027ba23d738eeb
adorahelen <adorahelenmin@gmail.com>
2024-08-14 22:53:53 +0900

    악마의 백틱

--- src/main/resources/static/article.js
+++ src/main/resources/static/article.js
@@ -12,3 +12,28 @@ if (deleteButton) {  // 'deletButton'을 'deleteButton'으로 수정
             });
     });
 }
+
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton) {
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+            .then(() => {
+                alert('Update Complete.');
+                location.replace(`/articles/${id}`);
+                // 와 아니 이거 백틱 아니라고 ㅋㅋㅋㅋ 와 아니 2시간을 와 아니 이게 와
+            });
+    });
+}
