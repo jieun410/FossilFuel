4880a1e9b84f251c27d1aa3aeed70128466f9581
adorahelen <adorahelenmin@gmail.com>
2024-08-20 23:28:33 +0900

    서비스: 인터페이스 구현

---
+++ src/main/java/edu/example/springbootblog/service/UserDetailService.java
@@ -0,0 +1,22 @@
+package edu.example.springbootblog.service;
+
+import edu.example.springbootblog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)   {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+    }// 오버라이딩 했지만, 신기한점은 loadUserByUsername 바꾸면 오류남 -> 원래 오버라이딩이 그럼
+
+
+}
