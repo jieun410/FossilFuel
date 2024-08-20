fcfb81bbeb68285fbde5c6d965e2271a46bccdb1
adorahelen <adorahelenmin@gmail.com>
2024-08-20 23:20:12 +0900

    엔티티, UserDetails: 사용자 인증정보 인터페이스

---
+++ src/main/java/edu/example/springbootblog/domain/User.java
@@ -0,0 +1,76 @@
+package edu.example.springbootblog.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Builder
+    public User(String email, String password, String auth) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override // 권한 반환 부분
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+        // 사용자가 가지고 있는 권한 목록을 반환한다.
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // 계정이 만료되었는지
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // password not expired?
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }// 계정이 아직 사용 가능한지
+}
