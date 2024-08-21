d8e86fb9cdb8c81ce0ebeff5e4b6e9a442744ee3
adorahelen <adorahelenmin@gmail.com>
2024-08-21 23:29:07 +0900

    사용자 정보 담는 객체(dto), apiCOn

---
+++ src/main/java/edu/example/springbootblog/controller/UserApiController.java
@@ -0,0 +1,21 @@
+package edu.example.springbootblog.controller;
+
+import edu.example.springbootblog.dto.securedto.AddUserRequest;
+import edu.example.springbootblog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Configuration
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(AddUserRequest addUserRequest) {
+        userService.save(addUserRequest); // 회원가입 메소드 호출 (서비스 단에 작성한)
+        return  "redirect:/login"; // 회원가입이 완료되면 로그인 페이지로 리 다이렉트
+    }
+
+}
