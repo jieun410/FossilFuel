f3d4ec114f0e82c11a97d737b012a1288da1b6c6
adorahelen <adorahelenmin@gmail.com>
2024-08-25 17:17:50 +0900

    메소드 위치 이동

--- src/main/java/edu/example/springbootblog/controller/UserViewController.java
+++ src/main/java/edu/example/springbootblog/controller/UserViewController.java
@@ -1,10 +1,5 @@
 package edu.example.springbootblog.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -19,12 +14,5 @@ public class UserViewController {
         return "signup";
     }
 
-    @GetMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response) {
-        new SecurityContextLogoutHandler().logout(request, response,
-                SecurityContextHolder.getContext().getAuthentication());
-
-        return "redirect:/login";
-    }
 }
 
