256de0b31e72573275c189daf555236851c6617d
adorahelen <adorahelenmin@gmail.com>
2024-08-25 16:47:06 +0900

    삭제 : h2설정

--- src/main/java/edu/example/springbootblog/config/WebSecurityConfig.java
+++ src/main/java/edu/example/springbootblog/config/WebSecurityConfig.java
@@ -11,25 +11,19 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-
 @RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
     private final UserDetailsService userService;
 
-    // 1. 스프링 스큐리티 기능 비활성화
+    // 1. 스프링 시큐리티에서 특정 리소스 무시 설정
     @Bean
-    public WebSecurityCustomizer configure(){
-        return (web)-> web.ignoring()
-                .requestMatchers(toH2Console())
-                .requestMatchers("/static/**");
-    }// 이유: 스프링 시큐리티 모든 기능은 무거워서, 인증 인가를 모든 서비스 적용 X
-    // * requestMatchers == 특정요청에 따른 url 액세스 설정
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/static/**"); // static 리소스에 대한 시큐리티 비활성화
+    }
 
-
-
-    // 2/ 특정 HTTP 요청에 대한 웹 기반 보안 구성
+    // 2. 특정 HTTP 요청에 대한 웹 기반 보안 구성
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -38,28 +32,24 @@ public class WebSecurityConfig {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin() // 4. form 기반 로그인 설정
-                .loginPage("/login")// 로그인 페이지 경로 지정
+                .loginPage("/login") // 로그인 페이지 경로 지정
                 .defaultSuccessUrl("/articles") // 로그인 성공시 이동할 경로 지정
                 .and()
                 .logout() // 5. 로그아웃 설정
                 .logoutSuccessUrl("/login")
                 .invalidateHttpSession(true)
                 .and()
-                .csrf().disable() // 6. CRSF 공격 방어, 비활성화
+                .csrf().disable() // 6. CSRF 보호 비활성화
                 .build();
-
-        // ()' is deprecated since version 6.1 and marked for removal
-        // 스프링 시큐리티 6.1 부터는 사용되지 않아서 제거될 예정이라는 경고 문구
     }
 
-    // 7. 인증 관리자 관련한 설정 들
+    // 7. 인증 관리자 관련한 설정
     @Bean
-    public AuthenticationManager authenticationManager
-    (HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder,
-     UserDetailsService userDetailsService ) throws Exception {
+    public AuthenticationManager authenticationManager(
+            HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder,
+            UserDetailsService userDetailsService) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userService)
-                //  8. 사용자 정보 서비스 설정
+                .userDetailsService(userService) // 8. 사용자 정보 서비스 설정
                 .passwordEncoder(bCryptPasswordEncoder)
                 .and()
                 .build();
@@ -71,5 +61,3 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
-
-
