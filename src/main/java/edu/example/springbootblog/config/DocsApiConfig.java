9ab473a6e56f0f99ca47d4bce2c5281f0182691c
adorahelen <adorahelenmin@gmail.com>
2024-08-28 21:31:17 +0900

    스웨거를 통한 api문서 작성

---
+++ src/main/java/edu/example/springbootblog/config/DocsApiConfig.java
@@ -0,0 +1,27 @@
+package edu.example.springbootblog.config;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info=@Info (
+                title = "REST API",
+                version = "ver 0.1",
+                description = "RESTful API Documentation"
+        ),
+        servers = { @Server(
+                description = "Prod ENV",
+                url = "http://localhost:8060/"
+        ),
+                @Server(
+                        description = "Staging ENV",
+                        url = "http://localhost:8060/staging"
+                )
+        }
+)
+public class DocsApiConfig {
+}
+
+
+
+
