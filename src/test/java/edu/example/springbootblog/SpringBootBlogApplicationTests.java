a2ef32acfcd3fa3f9b0c903b5c10f024591ef909
adorahelen <adorahelenmin@gmail.com>
2024-11-12 15:48:51 +0900

    ci 를 위한 깃허브 액션 준비

--- src/test/java/edu/example/springbootblog/SpringBootBlogApplicationTests.java
+++ src/test/java/edu/example/springbootblog/SpringBootBlogApplicationTests.java
@@ -2,8 +2,10 @@ package edu.example.springbootblog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:/application.properties")
 class SpringBootBlogApplicationTests {
 
     @Test
@@ -11,3 +13,4 @@ class SpringBootBlogApplicationTests {
     }
 
 }
+
